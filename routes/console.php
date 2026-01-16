dff79aaedbe8e8554b9bb22f950239f58d2c30cc
Oluwatobi Ishola <ishoshot@gmail.com>
2026-01-16 20:39:02 +0100

    refactor(routes): update console routes

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,13 @@
 <?php
 
 declare(strict_types=1);
+
+use App\Jobs\Briefings\CleanupExpiredBriefings;
+use App\Jobs\Briefings\GenerateScheduledBriefings;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('usage:aggregate')->hourly()->withoutOverlapping()->onOneServer();
+
+// Briefings scheduled jobs
+Schedule::job(new GenerateScheduledBriefings)->everyFiveMinutes()->withoutOverlapping()->onOneServer();
+Schedule::job(new CleanupExpiredBriefings)->daily()->withoutOverlapping()->onOneServer();
