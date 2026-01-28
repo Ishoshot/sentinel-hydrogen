d18f5a7fc7fa5e906915085d247bfed5ddbba9e3
Oluwatobi Ishola <ishoshot@gmail.com>
2026-01-28 12:34:46 +0100

    refactor(emails): reduce header logo width from 300px to 220px

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/sentinel-logo-wordmark.png') }}" class="logo" alt="{{ config('app.name') }}" style="width: 300px; height: auto;">
+<img src="{{ asset('images/sentinel-logo-wordmark.png') }}" class="logo" alt="{{ config('app.name') }}" style="width: 220px; height: auto;">
 </a>
 </td>
 </tr>
