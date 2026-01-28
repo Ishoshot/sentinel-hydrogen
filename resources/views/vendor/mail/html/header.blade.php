cf444e86b3650847b8be46854fcf0847def2a7c7
Oluwatobi Ishola <ishoshot@gmail.com>
2026-01-28 11:14:42 +0100

    refactor(emails): update header logo to use max-width constraint instead of fixed height

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('images/sentinel-logo-wordmark.png') }}" class="logo" alt="{{ config('app.name') }}" style="height: 40px;">
+<img src="{{ asset('images/sentinel-logo-wordmark.png') }}" class="logo" alt="{{ config('app.name') }}" style="max-width: 180px; height: auto;">
 </a>
 </td>
 </tr>
