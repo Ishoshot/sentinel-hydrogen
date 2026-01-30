f937c0bbf8565cc76bcd332e8a673ea8c30d3715
Oluwatobi Ishola <ishoshot@gmail.com>
2026-01-30 23:40:16 +0100

    feat(tests): add Architecture tests

--- tests/Arch/ExceptionsTest.php
+++ tests/Arch/ExceptionsTest.php
@@ -4,10 +4,31 @@ declare(strict_types=1);
 
 arch('exceptions')
     ->expect('App\Exceptions')
-    ->toImplement('Throwable')
-    ->toOnlyBeUsedIn([
+    ->not->toBeUsed()
+    ->ignoring([
+        'App\Exceptions\Rendering',
         'App\Actions',
         'App\Console\Commands',
         'App\Http\Controllers',
         'App\Services',
     ]);
+
+arch('exception classes implement throwable')
+    ->expect('App\Exceptions\NoProviderKeyException')
+    ->toImplement('Throwable');
+
+arch('exception renderers implement contract')
+    ->expect('App\Exceptions\Rendering\InvalidArgumentJsonRenderer')
+    ->toImplement('App\Exceptions\Rendering\ExceptionRenderer');
+
+arch('exception renderers implement contract - oauth')
+    ->expect('App\Exceptions\Rendering\OAuthCallbackRedirectRenderer')
+    ->toImplement('App\Exceptions\Rendering\ExceptionRenderer');
+
+arch('exception renderers implement contract - webhook')
+    ->expect('App\Exceptions\Rendering\WebhookSignatureRenderer')
+    ->toImplement('App\Exceptions\Rendering\ExceptionRenderer');
+
+arch('exception renderers implement contract - billing')
+    ->expect('App\Exceptions\Rendering\BillingRuntimeExceptionRenderer')
+    ->toImplement('App\Exceptions\Rendering\ExceptionRenderer');
